4a16fbc713ddc135a7f1ea4061c0da0cd3d2db79
Michael Bending <michael.a.bending@gmail.com>
2025-10-08 20:04:14 -0400

    removed ANOVA model check

--- regression/model_comparison.py
+++ regression/model_comparison.py
@@ -1,4 +1,3 @@
-import numpy as np
 from scipy.stats import f
 from dataclasses import dataclass
 from functools import cached_property
@@ -14,28 +13,6 @@ class ANOVA:
 
     full_model: LinearModel
     reduced_model: LinearModel
-
-    def __post_init__(self):  # TODO: refactor this to be more pythonic
-        if not np.array_equal(self.full_model.y_data, self.reduced_model.y_data):
-            raise Exception("y_data must match between full model and reduced model")
-        full_model_predictors = [
-            self.full_model.x_data[:, i] for i in range(self.full_model.predictor_count)
-        ]
-        reduced_model_predictors = [
-            self.reduced_model.x_data[:, i]
-            for i in range(self.reduced_model.predictor_count)
-        ]
-        for index, predictor in enumerate(reduced_model_predictors):
-            found = False
-            for fm_predictor in full_model_predictors:
-                if np.allclose(fm_predictor, predictor):
-                    found = True
-                    break
-            if not found:
-                raise Exception(
-                    f"every column of reduced_model.x_data has to be in full_model.x_data:"
-                    f"column_index={index}"
-                )
 
     @cached_property
     def f_score(self) -> float:
