f2f7d753fc2c60b95638ad80c5de5ff9a85b3cba
Michael Bending <michael.a.bending@gmail.com>
2025-10-18 11:15:47 -0400

    updated doc strings for plotting.py

--- regression/utils/plotting.py
+++ regression/utils/plotting.py
@@ -6,7 +6,7 @@ def auto_size_subplots(subplot_count: int) -> tuple[int, int]:
     automatically determine the optimal subplot size from how many subplots there are
 
     :param subplot_count: how many subplots you need
-    :return:
+    :return: the subplot size
     """
 
     max_diff = math.ceil(math.log10(subplot_count))
